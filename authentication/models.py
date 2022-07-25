60f754219b1c277d98f095b86ee43695611075c8
DESKTOP-P1I33JU\admin <katkat.w@hotmail.com>
2022-07-25 16:33:41 +0700

    unit test

--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import (PermissionsMixin, AbstractBaseUser, UserManager)
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils import timezone
+from django.apps import apps
+from django.contrib.auth.hashers import make_password
 from django.utils.translation import gettext_lazy as _
 
 from helpers.models import TrackingModel
